09470ff2910c62100489b2a8662b821dfa6a4a41
Przemek Dudek <przemod77@gmail.com>
2024-02-22 22:24:36 +0100

    Updated build.sql

--- Types/Shelter.sql
+++ Types/Shelter.sql
@@ -1,36 +1,84 @@
+CREATE TYPE Dog_type_array AS TABLE OF Dog_type;
+CREATE TYPE Employee_type_array AS TABLE OF Employee_type;
+
+CREATE SEQUENCE Shelter_sequence START WITH 1 INCREMENT BY 1;
+
 CREATE OR REPLACE TYPE Shelter_type AS OBJECT (
     ID INT,
-
     opening_hour VARCHAR2(10),
     closing_hour VARCHAR2(10),
-
     address Address_type,
+    feed_stock INT,
+    dogs Dog_type_array,
+    employees Employee_type_array,
 
     CONSTRUCTOR FUNCTION Shelter_type (
-        p_ID INT,
         p_opening_hour VARCHAR2,
         p_closing_hour VARCHAR2,
         p_street VARCHAR2,
         p_city VARCHAR2,
-        p_number VARCHAR2
-    ) RETURN SELF AS RESULT
-);
+        p_number INT
+    ) RETURN SELF AS RESULT,
+
+    MEMBER FUNCTION is_valid RETURN BOOLEAN,
+    STATIC FUNCTION create_shelter(
+        p_opening_hour VARCHAR2,
+        p_closing_hour VARCHAR2,
+        p_street VARCHAR2,
+        p_city VARCHAR2,
+        p_number INT
+    ) RETURN Shelter_type
+)
+NESTED TABLE dogs STORE AS dogs_table
+NESTED TABLE employees STORE AS employees_table;
 
 CREATE OR REPLACE TYPE BODY Shelter_type AS
     CONSTRUCTOR FUNCTION Shelter_type (
-        p_ID INT,
         p_opening_hour VARCHAR2,
         p_closing_hour VARCHAR2,
         p_street VARCHAR2,
         p_city VARCHAR2,
-        p_number VARCHAR2
+        p_number INT
     ) RETURN SELF AS RESULT IS
     BEGIN
-        ID := p_ID;
+        ID := Shelter_sequence.NEXTVAL;
         opening_hour := p_opening_hour;
         closing_hour := p_closing_hour;
         address := Address_type(p_street, p_city, p_number);
+        dogs := Dog_type_array();
+        employees := Employee_type_array();
         RETURN;
+    END;
+
+    MEMBER FUNCTION is_valid RETURN BOOLEAN IS
+    BEGIN
+        RETURN address IS NOT NULL AND opening_hour IS NOT NULL
+               AND closing_hour IS NOT NULL AND dogs IS NOT NULL
+               AND employees IS NOT NULL;
+    END;
+
+    STATIC FUNCTION create_shelter(
+        p_opening_hour VARCHAR2,
+        p_closing_hour VARCHAR2,
+        p_street VARCHAR2,
+        p_city VARCHAR2,
+        p_number INT
+    ) RETURN Shelter_type IS
+        new_shelter Shelter_type;
+    BEGIN
+        new_shelter := Shelter_type(
+            p_opening_hour,
+            p_closing_hour,
+            p_street,
+            p_city,
+            p_number
+        );
+
+        IF NOT new_shelter.is_valid THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Invalid shelter data');
+        END IF;
+
+        RETURN new_shelter;
     END;
 END;
 /
--- build.sql
+++ build.sql
@@ -1,9 +1,124 @@
 --### TYPES ###
 
+CREATE OR REPLACE TYPE Address_type AS OBJECT (
+    street VARCHAR2(100),
+    city VARCHAR2(100),
+    number_ INT
+);
 
--- Client, Dog, Employee
+CREATE SEQUENCE Client_sequence START WITH 1 INCREMENT BY 1;
 
--- Adoption
+CREATE OR REPLACE TYPE Client_type AS OBJECT (
+    ID INT,
+    Name VARCHAR(100),
+    Surname VARCHAR(100),
+    Phone VARCHAR(15)
+);
+
+CREATE SEQUENCE Employee_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TYPE Employee_type AS OBJECT (
+    ID INT,
+    name VARCHAR(100),
+    surname VARCHAR(100),
+    salary FLOAT,
+    date_of_hire DATE
+);
+
+CREATE SEQUENCE Adoption_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TYPE Adoption_type AS OBJECT (
+    ID INT,
+    dog_ID INT,
+    client_ID INT,
+    employee_ID INT,
+    status VARCHAR(20),
+
+    MEMBER FUNCTION is_valid RETURN BOOLEAN,
+    STATIC FUNCTION create_adoption(
+        p_dog_ID INT, p_client_ID INT, p_employee_ID INT, p_status VARCHAR
+    ) RETURN Adoption_type
+);
+/
+
+CREATE OR REPLACE TYPE BODY Adoption_type AS
+    MEMBER FUNCTION is_valid RETURN BOOLEAN IS
+    BEGIN
+        IF dog_ID IS NULL OR client_ID IS NULL OR employee_ID IS NULL THEN
+            RETURN FALSE;
+        END IF;
+
+        IF status NOT IN ('Rozpoczęta', 'Procesowanie', 'Zakończona') THEN
+            RETURN FALSE;
+        END IF;
+
+        RETURN TRUE;
+    END;
+
+    STATIC FUNCTION create_adoption(
+        p_dog_ID INT, p_client_ID INT, p_employee_ID INT, p_status VARCHAR
+    ) RETURN Adoption_type IS
+        v_adoption Adoption_type;
+    BEGIN
+        v_adoption := Adoption_type(
+            Adoption_sequence.NEXTVAL, p_dog_ID, p_client_ID, p_employee_ID, p_status
+        );
+
+        IF NOT v_adoption.is_valid THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Invalid adoption data');
+        END IF;
+
+        RETURN v_adoption;
+    END;
+END;
+/
+
+CREATE SEQUENCE Dog_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TYPE Dog_type AS OBJECT (
+    ID INT,
+    race VARCHAR(100),
+    age INT,
+    name VARCHAR(100),
+    status VARCHAR(20),
+    weight FLOAT,
+
+    MEMBER FUNCTION is_valid RETURN BOOLEAN,
+    STATIC FUNCTION create_dog(
+        p_race VARCHAR, p_age INT, p_name VARCHAR, p_status VARCHAR, p_weight FLOAT
+    ) RETURN Dog_type
+);
+/
+
+CREATE OR REPLACE TYPE BODY Dog_type AS
+    MEMBER FUNCTION is_valid RETURN BOOLEAN IS
+    BEGIN
+        IF race IS NULL OR age IS NULL OR name IS NULL OR status IS NULL OR weight IS NULL THEN
+            RETURN FALSE;
+        END IF;
+
+        RETURN TRUE;
+    END;
+
+    STATIC FUNCTION create_dog(
+        p_race VARCHAR, p_age INT, p_name VARCHAR, p_status VARCHAR, p_weight FLOAT
+    ) RETURN Dog_type IS
+        v_dog Dog_type;
+    BEGIN
+        v_dog := Dog_type(
+            Dog_sequence.NEXTVAL, p_race, p_age, p_name, p_status, p_weight
+        );
+
+        IF NOT v_dog.is_valid THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Invalid dog data');
+        END IF;
+
+        RETURN v_dog;
+    END;
+END;
+/
+
+--SHELTER
 
 --### CREATE TABLES ###
 
@@ -16,6 +131,10 @@ CREATE TABLE Employees_Table  OF EMPLOYEE_TYPE (PRIMARY KEY (ID));
 CREATE TABLE Client_Table OF CLIENT_TYPE (PRIMARY KEY (ID));
 
 CREATE TABLE Dog_Table OF DOG_TYPE (PRIMARY KEY (ID));
+
+CREATE TABLE Shelter_Table OF Shelter_type (PRIMARY KEY (ID));
+
+
 
 
 
